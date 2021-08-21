ef8e382af9c8bab2a1459cca59e17398d95a167f
iCode_Rx <arababs2015@gmail.com>
2021-08-21 12:44:01 +0100

    moving

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,24 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
-
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
-
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
-
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
-
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
+//Select element function
+const selectElement = function(element) {
+        return document.querySelector(element);
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
 
-    }catch(err){
-        console.log(err)
-    }
-}
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+menuToggler.addEventListener('click', function() {
+        body.classList.toggle('open');
+})
 
-    }catch(err){
-        console.log(err)
-    }
+let displayInfo = selectElement(".show-text")
+
+let btn = selectElement(".submit")
+
+btn.addEventListener("click", func)
+
+function func() {
+        let firstName = selectElement(".first").value
+
+        displayInfo.innerHTML = `Hello, ${firstName}, Babatunde will get back to you soon. Thanks`
 }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const PORT = 2021
 require('dotenv').config()
 
 
 let db,
 	dbConnectionStr = process.env.DB_STRING,
-	dbName = 'todo'
+	dbName = 'hng'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 	.then(client => {
@@ -21,70 +21,29 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{
-	const todoItems = await db.collection('todos').find().toArray()
-	const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-	response.render('index.ejs', { items: todoItems, left: itemsLeft })
-	// db.collection('todos').find().toArray()
-	// .then(data => {
-	//     db.collection('todos').countDocuments({completed: false})
-	//     .then(itemsLeft => {
-	//         response.render('index.ejs', { items: data, left: itemsLeft })
-	//     })
-	// })
-	// .catch(error => console.error(error))
+app.get('/', async (req, res)=>{
+	try {		
+		res.render('index.html')
+	} catch (err) {
+		res.status(500).json(err)
+	}
 })
 
-app.post('/addTodo', (request, response) => {
-	db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-	.then(result => {
-		console.log('Todo Added')
-		response.redirect('/')
-	})
-	.catch(error => console.error(error))
-})
-
-app.put('/markComplete', (request, response) => {
-	db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-		$set: {
-		completed: true
+app.post('/contact', (req, res) => {
+	db.collection('zuri').insertOne(
+		{
+			userFirstname: req.body.userFirstname,
+			userLastname: req.body.userLastname,
+			userEmail: req.body.userEmail,
 		}
-	},{
-		sort: {_id: -1},
-		upsert: false
-	})
-	.then(result => {
-		console.log('Marked Complete')
-		response.json('Marked Complete')
-	})
-	.catch(error => console.error(error))
+	)
+		.then(result => {
+			res.redirect('/')
+		})
+
+		.catch(error => console.error(error))
 })
 
-app.put('/markUnComplete', (request, response) => {
-	db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-		$set: {
-		completed: false
-		}
-	},{
-		sort: {_id: -1},
-		upsert: false
-	})
-	.then(result => {
-		console.log('Marked Complete')
-		response.json('Marked Complete')
-	})
-	.catch(error => console.error(error))
-})
-
-app.delete('/deleteItem', (request, response) => {
-	db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-	.then(result => {
-		console.log('Todo Deleted')
-		response.json('Todo Deleted')
-	})
-	.catch(error => console.error(error))
-
-})
 
 app.listen(process.env.PORT || PORT, ()=>{
 	console.log(`Server running on port ${PORT}`)
